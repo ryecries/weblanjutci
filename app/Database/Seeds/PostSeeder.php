50a25f6d8281b4cf80f69b85f6a0217d00e4d1b6
ryecries <you@example.com>
2021-09-30 13:12:41 +0700

    update

--- app/Database/Seeds/PostSeeder.php
+++ app/Database/Seeds/PostSeeder.php
@@ -11,13 +11,14 @@ class PostSeeder extends Seeder
 		$model = model('PostModel');
 
 		$kategori = ['nature', 'programming', 'cat', 'sport'];
-
-		$model->insert([
-			'judul'		=> static::faker()->sentence(3),
-			'slug'		=> static::faker()->unique()->slug(3),
-			'author'	=> static::faker()->name(3),
-			'kategori'	=> $kategori[0],
-			'deskripsi'	=> static::faker()->text(),
-		]);
+		for ($i = 0; $i < 6; $i++) {
+			$model->insert([
+				'judul'		=> static::faker()->sentence(3),
+				'slug'		=> static::faker()->unique()->slug(3),
+				'author'	=> static::faker()->name(3),
+				'kategori'	=> $kategori[0],
+				'deskripsi'	=> static::faker()->text(),
+			]);
+		}
 	}
 }
